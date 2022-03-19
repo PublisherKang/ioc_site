1bb1a74212866eed98f14af064b3afcf8e8bbf78
desktop-kang <kbs21c1987@gmail.com>
2022-03-20 01:44:19 +0900

    add

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // pc 메뉴 이동
+    var nav = $(".gnb ul li");
+    var cont = $("#container .nav_go");
+
+    nav.click(function(c){
+        c.preventDefault();
+        var target = $(this);
+        var index = target.index();
+
+        var section = cont.eq(index);
+        var offset = section.offset().top;
+        
+        $("html, body").stop().animate({ scrollTop: offset },500);
+        
+        
+    });
+});
